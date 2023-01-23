a5e31cd6f9262c7ca63804678d57ee6d48aeeb30
Olive Provencio-Johnson <120212494+Olive-Provencio-Johnson@users.noreply.github.com>
2023-01-22 21:24:51 -0800

    added pseudo code based on user story and criteria

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+//Create start button 
+//start button starts a timer 
+
+//First question is presented after timer is started 
+
+//When a question is answered, the next question is presented 
+
+//When a question is answered incorrectly, time is subtracted from the clock 
+
+
+//The game is OVER when all questions are answered OR the timer reaches 0
+
+//When the game is over, the user can save their initials AND their score
