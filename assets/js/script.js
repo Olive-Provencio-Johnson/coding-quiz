e0047426cde488ccbfadc444aa449969355c1226
Olive Provencio-Johnson <120212494+Olive-Provencio-Johnson@users.noreply.github.com>
2023-01-22 21:39:58 -0800

    added variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
+var timer = 76;
+var start = ""; 
+var highscores = "";
+var goBack = ""; 
+
+
 //Create start button 
+
 //start button starts a timer 
 
 //First question is presented after timer is started 
